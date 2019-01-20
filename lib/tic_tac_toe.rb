128822a08714f7cc5c10f8742c6e5000d4fe2896
Abhijit Roy <aroy30@uic.edu>
2019-01-20 15:36:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,6 +4,31 @@ class TicTacToe # class starts
     @board = Array.new(9, " ")
   end
 
+  # Nested Array of each Win combination
+  WIN_COMBINATIONS = [    
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  def display_board
+    puts 
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+
+
+
+  
 
 
 end # class ends here
